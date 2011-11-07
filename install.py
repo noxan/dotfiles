7e58665ce16c114b85c572c6f296713eaa7cdc6d
noxan <noxan@redmoonstudios.de>
2011-11-07 09:21:08 +0100

    list files in installscript and copy filtered files and directories

--- install.py
+++ install.py
@@ -7,3 +7,13 @@ srcpath = os.path.abspath(os.path.dirname(__file__))
 dstpath = os.path.abspath(os.path.expanduser("~"))
 
 print "Installing dotfiles from "+str(srcpath)+" to "+str(dstpath)+" ..."
+
+dirlist = os.listdir(srcpath)
+for f in dirlist:
+    if f != '.git' and f != '.gitignore' and f != 'install.py':
+        if os.path.isdir(f):
+            os.system("cp "+os.path.join(srcpath, f)+" "+os.path.join(dstpath, f)+" -r")
+            print "Copying directory "+f+" ..."
+        else:
+            os.system("cp "+os.path.join(srcpath, f)+" "+os.path.join(dstpath, f))
+            print "Copying file "+f+" ..."
