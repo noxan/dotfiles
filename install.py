36d6d6d7cd8eab2fa33f272abd606151a09acae1
noxan <noxan@redmoonstudios.de>
2011-11-07 08:51:41 +0100

    get directories in install script and display info

--- install.py
+++ install.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python
 
+import os
 
+
+srcpath = os.path.abspath(os.path.dirname(__file__))
+dstpath = os.path.abspath(os.path.expanduser("~"))
+
+print "Installing dotfiles from "+str(srcpath)+" to "+str(dstpath)+" ..."
