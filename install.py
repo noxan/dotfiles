786a3bba2fc6651280e89e6379abd4be01c2586d
noxan <noxan@redmoonstudios.de>
2012-02-01 17:59:41 +0100

    rework install script to link files and only those that do not already exist

--- install.py
+++ install.py
@@ -6,14 +6,15 @@ import os
 srcpath = os.path.abspath(os.path.dirname(__file__))
 dstpath = os.path.abspath(os.path.expanduser("~"))
 
-print "Installing dotfiles from "+str(srcpath)+" to "+str(dstpath)+" ..."
+print "Installing dotfiles from '"+str(srcpath)+"' to '"+str(dstpath)+"':"
 
 dirlist = os.listdir(srcpath)
 for f in dirlist:
     if f != '.git' and f != '.gitignore' and f != 'install.py':
-        if os.path.isdir(f):
-            os.system("cp "+os.path.join(srcpath, f)+" "+os.path.join(dstpath, "."+f)+" -r")
-            print "Copying directory "+f+" ..."
-        else:
-            os.system("cp "+os.path.join(srcpath, f)+" "+os.path.join(dstpath, "."+f))
-            print "Copying file "+f+" ..."
+        srcfile = os.path.join(srcpath, f)
+        dstfile = os.path.join(dstpath, "."+f)
+        print "> "+f
+        if not os.path.exists(dstfile):
+            os.system("ln -s "+srcfile+" "+dstfile)
+
+print "Done."
