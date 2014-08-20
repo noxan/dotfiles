4267ee287b514999525f7478aaf0c570542151e4
noxan <noxan@byteweaver.net>
2014-08-20 21:17:57 +0200

    improve install script and add handling for i3 files

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+set -e
 
-ln -s vim/vim ~/.vim
-ln -s vim/vimrc ~/.vimrc
+if [ ! -n "$DOTFILES" ]; then
+  DOTFILES=~/.dotfiles
+fi
+
+
+# vim
+ln -s "$DOTFILES/vim/vim/" ~/.vim
+ln -s "$DOTFILES/vim/vimrc" ~/.vimrc
+# i3
+ln -s "$DOTFILES/i3/i3/" ~/.i3
+ln -s "$DOTFILES/i3/i3status.conf" ~/.i3status.conf
