43178d24b438b9f69476f8af0c4defc5252a6c9f
noxan <noxan@byteweaver.net>
2014-08-21 10:31:31 +0200

    improve uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-rm -f ~/.vim
-rm -f ~/.vimrc
-rm -f ~/.gitconfig
-rm -f ~/.i3
-rm -f ~/.i3status.conf
+function uninstallConfig {
+  rm -rf "$HOME/.$1"
+}
+# vim
+uninstallConfig 'vim'
+uninstallConfig 'vimrc'
+# git
+uninstallConfig 'gitconfig'
+# i3
+uninstallConfig 'i3/'
+uninstallConfig 'i3status.conf'
+# Xorg
+uninstallConfig 'xinitrc'
+uninstallConfig 'Xmodmap'
+uninstallConfig 'Xresources'
+# python
+uninstallConfig 'pydistutils.cfg'
+# gtk
+uninstallConfig 'gtkrc-2.0'
