0d21c33e624ae3dd05225fa65c9bc4ab25b15ffa
Richard <hello@noxan.net>
2017-11-29 18:11:47 +0100

    use fish as shell

--- dotfiles/hyper.js
+++ dotfiles/hyper.js
@@ -86,7 +86,7 @@ module.exports = {
     //
     // Powershell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
+    shell: '/usr/local/bin/fish',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
