d8d3cd7ebee591b21aa9f36ac4dd563724106f90
Richard <hello@noxan.net>
2018-01-28 20:23:22 +0100

    add fish config file

---
+++ dotfiles/fish/config.fish
@@ -0,0 +1,26 @@
+set fish_greeting ""
+
+# Base16 Shell
+# if status --is-interactive
+#   eval sh $HOME/.config/base16-shell/scripts/base16-oceanicnext.sh
+# end
+
+export EDITOR=nvim
+
+abbr -a s "cd $HOME/workspace"
+
+abbr -a vi "nvim"
+abbr -a vim "nvim"
+
+abbr -a g "git"
+
+# abbr -a code "code-insiders"
+
+export PATH="$HOME/workspace/flutter/bin:$PATH"
+
+# echo $PATH
+
+# Android
+# export ANDROID_HOME="$HOME/Library/Android/sdk"
+# export PATH="$PATH:$ANDROID_HOME/tools"
+# export PATH="$PATH:$ANDROID_HOME/platform-tools"
