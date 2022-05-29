94552ebca6cffc41525566a8aec0c28555dd28fa
Richard <richard@stromer.org>
2022-05-29 21:15:12 +0200

    add newer fish config (to be merged)

---
+++ dotfiles/fish/config2.fish
@@ -0,0 +1,10 @@
+export PATH="$PATH:/usr/local/bin/"
+
+set fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+end
+
+export EDITOR=vim
